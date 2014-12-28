dba0a7b75a03483f8bda5efda859f6caec79d9e1
Will Huxtable <will.huxtable@gmail.com>
2014-12-28 12:39:06 +0000

    Updated for colour scheme

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir -p ~/.vim/colors
+curl -o ~/.vim/colors/solarized.vim https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
